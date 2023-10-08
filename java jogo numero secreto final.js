696cfc4ef5e8f78d45d6b2cf66daad6270eeb449
rafoux235 <146496920+rafoux235@users.noreply.github.com>
2023-10-07 21:34:53 -0300

    Add files via upload

---
+++ java jogo numero secreto final.js
@@ -0,0 +1,68 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+campo.innerHTML = texto 
+}
+
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1', 'Jogo Do Número Secreto');
+    exibirTextoNaTela('p', 'Escolha um Número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else{
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('p', 'o número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'o número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio(){ 
+   return parseInt(Math.random() * 10 + 1 );
+}
+
+function limparCampo (){
+   chute = document.querySelector('input');
+   chute.value = '';
+}
+
+function reiniciarJogo(){
+  numeroSecreto = gerarNumeroAleatorio();
+  limparCampo();
+  tentativas = 1;
+  exibirMensagemInicial();
+  document.getElementById('reiniciar').setAttribute('disabled', true)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
